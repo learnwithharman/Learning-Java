ea33cfeca90ab06f9aa7086dd64edf7cadf12660
Harman <officialharman69@gmail.com>
2026-01-01 18:15:56 +0530

    Day 1: Added first Java program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    //this is a simple java program
+    /*
+    we can use sout as shortcut of system.out.println */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first java program");
+    }
+}
