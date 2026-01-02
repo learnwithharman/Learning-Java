a44572d0c14b39a29e82006142037eeeeaf4e30c
Harman <officialharman69@gmail.com>
2026-01-03 00:36:25 +0530

    Added program to calculate sum of two numbers without user input

---
+++ SumTwoNumbers.java
@@ -0,0 +1,11 @@
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum of two numbers = " + sum);
+    }
+}
