2f9e02614ddf4eafdb0823176e17972161ab8fb0
Harman <officialharman69@gmail.com>
2026-01-05 20:20:23 +0530

    Learning variables

---
+++ variables.java
@@ -0,0 +1,20 @@
+public class variables {
+    public static void main(String[] args){
+
+        int age = 19;
+        String name = "Harman";
+        double height = 6.1;
+        double collegefees = 499.99;
+        char grade = 'A';
+        boolean ispassed = true;
+
+        System.out.println("My name is " + name + " and my age is " + age);
+        System.out.println("I have updated my age to 20");
+        System.out.println("my height is "+ height +"feet");
+        System.out.println("My college fees is "+"$"+ collegefees);
+        System.out.println("I got " + grade + " grade in my last exam");
+        System.out.println("Is it true that I have passed my exam? "+ ispassed);
+
+    }
+    
+}
