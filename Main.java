7ac6017bed7410d7c84afdd8d1e8655e8899e644
Harman <officialharman69@gmail.com>
2026-01-07 23:31:57 +0530

    Scanner use and Some explantory notes

--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
+// Importing Scanner class from java.util package
+// Scanner is used to take input from the user (keyboard)
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        // Creating Scanner object to read input from keyboard
         Scanner scanner = new Scanner(System.in);
+
+        // Asking user to enter name
         System.out.println("Enter your name :");
+
+        // nextLine() is used to read a full line of text (String)
+        // It reads input until the user presses Enter
         String name = scanner.nextLine();
+
+        // Asking user to enter age
         System.out.println("Enter your age : ");
+
+        // nextInt() is used to read an integer value
+        // It only reads numbers (int)
         int age = scanner.nextInt();
-        System.out.println("Hello, " + name + "! Welcome to the Simulation World"); 
+
+        // Printing greeting message using String concatenation
+        System.out.println("Hello, " + name + "! Welcome to the Simulation World");
         System.out.println("You are " + age + " years old");
 
+        // Closing the scanner to free system resources
         scanner.close();
     }
-
-
 }
