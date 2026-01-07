6ee16ece77fd2a8797eee8d0e8376282c90727df
Harman <officialharman69@gmail.com>
2026-01-07 23:30:20 +0530

    Scanner

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age : ");
+        int age = scanner.nextInt();
+        System.out.println("Hello, " + name + "! Welcome to the Simulation World"); 
+        System.out.println("You are " + age + " years old");
+
+        scanner.close();
+    }
+
+
+}
