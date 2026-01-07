3bcf1d5be7ca7b5530385beb9fe610446cad0b7a
Harman <officialharman69@gmail.com>
2026-01-08 00:12:32 +0530

    Rectangle area calculator and Square identifier

---
+++ areaofrectangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class areaofrectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        double area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+
+        if (length == width) {
+            System.out.println("This rectangle is also a square.");
+        } else {
+            System.out.println("This rectangle is not a square.");
+        }
+
+        scanner.close();
+    }
+    
+}
