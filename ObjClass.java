619d6d7c62997e66340e531a65499a3d2f0ef239
Harman <officialharman69@gmail.com>
2026-01-20 23:12:03 +0530

    Classes in Java

--- ObjClass.java
+++ ObjClass.java
@@ -1,14 +1,37 @@
+// This is a normal class (blueprint)
 class Calculator {
+
+    /*
+     * Method:
+     * - Performs addition
+     * - Takes two integers as input
+     * - Returns integer result
+     */
     public int add(int num1, int num2) {
-        int r = num1 + num2;
-        return r;
+        int r = num1 + num2;   // local variable
+        return r;              // return result to caller
     }
 }
+
+// This class contains main() method → program starts here
 public class ObjClass {
+
     public static void main(String[] args) {
+
+        /*
+         * Object creation:
+         * Calculator = class
+         * calc = reference variable
+         * new Calculator() = object in memory
+         */
         Calculator calc = new Calculator();
-        int result = calc.add(4,5);
+
+        /*
+         * Method call using object:
+         * object.method(arguments)
+         */
+        int result = calc.add(4, 5);
+
         System.out.println("Result: " + result);
     }
-    
 }
