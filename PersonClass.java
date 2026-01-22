5938fe97a262c6fccaf3329880042acf5b02c243
Harman <officialharman69@gmail.com>
2026-01-22 21:14:37 +0530

    Understanding classes better

---
+++ PersonClass.java
@@ -0,0 +1,21 @@
+// Simple class
+class Person {
+
+    // method of class
+    void sayHello() {
+        System.out.println("Hello, I am a person.");
+    }
+}
+
+// Main class
+public class PersonClass {
+
+    public static void main(String[] args) {
+
+        // Creating object of Person class
+        Person p1 = new Person();
+
+        // Calling method using object
+        p1.sayHello();
+    }
+}
