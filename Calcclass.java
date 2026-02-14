35eeede59bcf8e5d229fe2b219b92056f35e98dd
Harman <officialharman69@gmail.com>
2026-02-14 21:49:35 +0530

    Calculator using Class, Methods

---
+++ Calcclass.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
+public class Calcclass
+{
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = calc.add(num1, num2);
+        int difference = calc.subtract(num1, num2);
+        int product = calc.multiply(num1, num2);
+        double quotient = calc.divide(num1, num2);
+        sc.close();
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
