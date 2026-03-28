5d88fbfdcf5d4ef2899e2b653f857cf389d85904
Vintage Monk <officialharman69@gmail.com>
2026-03-28 07:43:01 +0530

    Reversing an ARRAY

---
+++ reverseArr.java
@@ -0,0 +1,17 @@
+public class reverseArr {
+    public static void main (String[] args){
+        int [] arr = new int [5];
+        arr [0]= 1;
+        arr [1]= 2;
+        arr [2]= 3;
+        arr [3]= 4;
+        arr [4]= 5;
+
+        for (int i = arr.length-1; i>=0; i--){
+            System.out.println(arr[i]);
+
+        }    
+
+
+    }
+}
