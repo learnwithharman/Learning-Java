7ac468f60b88708866c2a738044a89ea2235d66a
Vintage Monk <officialharman69@gmail.com>
2026-04-05 20:31:48 +0530

    Recursion: Factorial fix

--- factorialrec.java
+++ factorialrec.java
@@ -1,17 +1,12 @@
 public class factorialrec {
-    public static void main (String[]args){
-        System.out.println(factorial(1));
-
-
+    public static void main(String[] args) {
+        System.out.println(factorial(5)); 
     }
-    public static int factorial(int n){
-        if (n==1){
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {   //Agar n = 0 diya toh infinite recursion / error
             return 1;
         }
-        else{
-            return n*factorial(n-1);
-        }
-
-}
-    
+        return n * factorial(n - 1);
+    }
 }
