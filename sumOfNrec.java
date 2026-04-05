cc6051058237b6f0006cccc549683715f1a37f2b
Vintage Monk <officialharman69@gmail.com>
2026-04-05 20:28:19 +0530

    Printing Sum of N natural numbers using Recursion

---
+++ sumOfNrec.java
@@ -0,0 +1,14 @@
+public class sumOfNrec {
+    public static void main (String[]args){
+        System.out.println(sum(5));
+
+    }
+    public static int sum(int n){
+        if (n==1){
+            return 1;
+        }
+        else{
+            return n+sum(n-1);
+        }
+        }
+}
